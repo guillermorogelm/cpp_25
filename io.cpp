4eb85dad967e94d6b262bb8db17a26339c8e160f
Guillermo <guillerogel@hotmail.com>
2026-02-25 08:15:40 -0500

    add hello world example and additional C++ files

---
+++ io.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    string nombre;
+
+    cout << "Ingresa nombre de Heroe: ";
+    cin >> nombre;
+
+    cout << "Bienvenido, " << nombre << "!" << endl;
+
+    return 0;
+}
